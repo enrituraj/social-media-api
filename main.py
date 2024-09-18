288ca13bd997214490fd13167d88b1d36ce0733e
Ritu <rn26rituraj@gmail.com>
2024-09-18 09:17:26 +0530

    created create post route and accept data from user

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message":"Welcome to my api"}
+
+@app.post("/createpost")
+def createpost(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post":f"title: {payload['title']},content:{payload['content']}"}
 
 
